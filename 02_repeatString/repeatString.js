75cf34230f1373d2d4d35c6eef36b34d8a5aa5b2
Kyle Furtado <kylefurtado34@gmail.com>
2024-01-02 21:35:10 -0500

    Completed challenge 1.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  let string = "";
+  if (times >= 0) {
+    for (i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,26 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const myArray = text.split("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+/*
+let input = "hello";
+
+text = input.split("");
+
+let newArray = []
+let a = 0;
+
+for (i = text.length - 1; i >= 0; i--) {
+  console.log(text[i]);
+  newArray[a] = text[i];
+  a++;
+}
+
+text = newArray.toString("");
+console.log(newArray.toString(""));
+console.log(text);
+*/
