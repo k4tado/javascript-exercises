c41a11bc85e384fef5aa9a183ec9683f4ee7ffa8
Kyle Furtado <kylefurtado34@gmail.com>
2024-01-03 21:16:24 -0500

    Completed challenge 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,26 +1,18 @@
-const reverseString = function(input) {
-    const myArray = text.split("");
+const reverseString = function (input) {
+  /*let newArray = [];
+  let a = 0;
+
+  for (let i = input.length - 1; i >= 0; i--) {
+    console.log(input[i]);
+    newArray[a] = input[i];
+    a++;
+  }
+  return newArray.join("");*/
+  
+  console.log(input.split("").reverse().join(""));
+  return input.split("").reverse().join("");
+  
 };
 
 // Do not edit below this line
 module.exports = reverseString;
-
-
-/*
-let input = "hello";
-
-text = input.split("");
-
-let newArray = []
-let a = 0;
-
-for (i = text.length - 1; i >= 0; i--) {
-  console.log(text[i]);
-  newArray[a] = text[i];
-  a++;
-}
-
-text = newArray.toString("");
-console.log(newArray.toString(""));
-console.log(text);
-*/
