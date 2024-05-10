bb258429cafc144d4efa15dab7e78dffb5001dc9
k4tado <kylefurtado34@gmail.com>
2024-05-09 21:36:24 -0400

    Made progress on problem 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,25 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for ( let i = 0; i < array.length; i++ ) {
+    sum += parseInt(array[i]);
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = array[0];
+	for ( let i = 1; i < array.length; i++ ) {
+    product *= parseInt(array[i]);
+  }
+  return product;
 };
 
 const power = function() {
