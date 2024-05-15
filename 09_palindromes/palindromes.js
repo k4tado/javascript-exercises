a685fa8c123701702191c23c443b300118c7e08d
Kyle Furtado <kylefurtado34@gmail.com>
2024-05-14 21:23:56 -0400

    Updated code for problem 09.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (prompt) {
+    let string = prompt.toLowerCase().split("`").reverse().join('')
+    let newString = prompt.toLowerCase().split("`").join('');
 
+    if ( string === newString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+/*
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+let prompt = "racecar"
+
+let string = prompt.toLowerCase().split("`").reverse().join('')
+
+let newString = prompt.toLowerCase().split("`").join('');
+
+console.log(string);
+console.log(newString);
+console.log(prompt);
+console.log(newString == prompt);
+*/
