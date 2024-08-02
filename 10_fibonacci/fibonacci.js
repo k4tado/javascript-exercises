a933297db462bf26ab2c60f959978b7a7743355f
k4tado <kylefurtado34@gmail.com>
2024-08-01 23:26:17 -0400

    Finished problem 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (given) {
 
+if (given < 0) {
+    return "OOPS";
+}
+
+  x = 1;
+  y = 1;
+
+  if (given == 1 || given == 2) {
+    return 1;
+  } else if (given == 0) {
+    return 0;
+  }
+
+  for (i = 2; i < given; i++) {
+    z = x + y;
+    placeholder = z;
+    x = y;
+    y = z;
+  }
+
+  return z;
 };
 
 // Do not edit below this line
